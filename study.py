54671fbc30f4d43833c13222a7f4ba5e536bc359
larilima06 <limnslima@gmail.com>
2025-01-15 21:33:20 -0300

    Add files via upload

---
+++ study.py
@@ -0,0 +1,24 @@
+nomeHéroi = input("Digite o nome do seu héroi: ")
+xp = int(input("Digite o nível de experiência: "))
+if xp == 1000:
+	xp = "ferro"
+if xp > 1000 and xp < 2000:
+	xp = "bronze"
+if xp == 2000:
+	xp = "prata"
+if xp == 4000:
+	xp = "ouro"
+if xp == 5000:
+	xp = "platina"
+if xp == 6000:
+	xp = "diamante"
+if xp == 7000:
+	xp = "mestre"
+if xp == 8000:
+	xp = "grão-mestre"
+if xp == 9000:
+	xp = "desafiante"
+if xp == 10000:
+	xp = "radiante"
+print("Olá, " + nomeHéroi + "! Seu nível de experiência é " + xp + ".")
+
